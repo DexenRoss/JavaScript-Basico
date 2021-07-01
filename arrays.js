0eafce0c3b5c27a9d87ed10ba5a4b07338023e31
emilio <emiliocaballero@ciencias.unam.mx>
2021-07-01 12:12:22 -0500

    Terminamos el curso viendo como modificar y recorrer arreglos

--- arrays.js
+++ arrays.js
@@ -26,3 +26,46 @@ while (arr.length>0) {
     contarFrutas(fruit); 
 }
 
+// recorrer arrays 
+
+var articulos = [
+    {nombre: "Bci", precio: 3000},
+    {nombre: "Tv", precio: 2500},
+    {nombre: "Libro", precio: 325},
+    {nombre: "Celular", precio: 10000},
+    {nombre: "Laptop", precio: 20000},
+    {nombre: "Teclado", precio: 500},
+    {nombre: "Audifonos", precio: 1700}
+];
+
+// recorrer con Filter
+
+var filtrarArticulos = articulos.filter(function(articulo){
+    return articulo.precio <= 500;
+});
+
+// metodo de map
+
+var nombreArticulos = articulos.map(function(articulo){
+    return articulo.nombre;
+});
+
+// metodo find
+
+var encuentraArticulo = articulos.find(function(articulo){
+    return articulo.nombre === "Laptop";
+});
+
+// metodo foreach
+
+articulos.forEach(function(articulo){
+    console.log(articulo.nombre);
+});
+
+// metodo some, regresa resultado booleano
+
+var articulosBaratos = articulos.some(function(articulo){
+    return articulo.precio <=700;
+});
+
+
